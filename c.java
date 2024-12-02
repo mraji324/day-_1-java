72631512b0cddad3e296d72073ba7982fbe006ae
mraji324 <rajuhindur@gmail.com>
2024-12-02 15:03:43 +0530

    Add files via upload

---
+++ c.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class c
+{
+    public static void main(String[] args)
+    {
+        int a=50;
+        char b='h';
+        float c=3.24f;
+        double d=3.45;
+        boolean e=true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
